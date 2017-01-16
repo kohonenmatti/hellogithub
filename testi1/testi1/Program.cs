1db190844787589dc31bb3a9c8e60678def2c77a
Matti Kohonen <kohonenmatti1994@gmail.com>
2017-01-16 19:30:40 +0200

    HELLOUUUU

---
+++ testi1/testi1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HELLOUUU GITHUBBIII!!!!!);
+            Console.ReadLine();
+
+
+        }
+    }
+}
